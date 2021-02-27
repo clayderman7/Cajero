0951106cc7847d120646ae9e995bead8bd079155
clayderman ortega <clayderman7@hotmail.com>
2021-02-27 13:05:09 -0500

    Proyecto casi terminado(se me olvido subirlo al iniciar ^_^')

---
+++ Cajero/Cajero.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using System.Linq;
+using System.IO;
+
+
+namespace Cajero
+{
+    class Cajero
+    {
+        private readonly string pathPassword;
+        private readonly string contraseña;
+        private double saldo;
+        static int count;
+
+        private readonly List<string> movimientos = new List<string>();
+        private readonly DateTime dateTime = DateTime.Now;
+        readonly TimeZoneInfo timeZone = TimeZoneInfo.Local;
+
+        public Cajero()
+        {
+            pathPassword = @"C:\Users\Los Ortegas\source\repos\Cajero\Folder pruebaFile\PwStore";
+            contraseña = File.ReadAllText(pathPassword);
+            saldo = 4000.00f;
+            count = 1;
+        }
+
+        public double GetSaldo()
+        {
+            String objList = string.Format("Consulta de Saldo. --> Fecha: {0:d}  Hora: {0:t} Zone: {1}.", dateTime, timeZone);
+            movimientos.Add(objList);
+            return saldo;
+        }
+
+        public void Deposit(double monto)
+        {
+            if (monto == 0 || monto > 500)
+            {
+                Console.WriteLine("\nEl monto de deposito excede el limite de deposito por cajero,\n-Monto Min(1)\n-Monto Max(500)");
+            }
+            else
+            {
+                String objList = string.Format("Deposito: {2:c}. --> Fecha: {0:d}  Hora: {0:t} Zone: {1}.", dateTime, timeZone, monto);
+                movimientos.Add(objList);
+                this.saldo += monto;
+            }
+        }
+
+        public void Withdraw(double saldo)
+        {
+            if (saldo > this.saldo)
+            {
+                if (saldo == 0) { Console.WriteLine("Monto minimo de retiro es $1"); }
+
+                Console.WriteLine("\nSaldo insuficiente");
+                String objList = string.Format("Intento de Retiro: {0:c}. Saldo Insuficiente. --> Fecha: {1:d}  Hora: {1:t} Zone: {2}. ", saldo, dateTime, timeZone);
+                movimientos.Add(objList);
+            }
+            else
+            {
+                String objList = string.Format("Retiro: {0:c}. --> Fecha: {1:d}  Hora: {1:t} Zone: {2}.", saldo, dateTime, timeZone);
+                movimientos.Add(objList);
+                this.saldo -= saldo;
+                Console.Clear();
+                Console.WriteLine($"\nHas retirado: {saldo:c}");
+            }
+        }
+
+        public void ChangePassword(string newPassword)
+        {
+            Console.WriteLine("Ingrese una nueva contraseña");
+            File.WriteAllText(pathPassword, newPassword);
+
+        }
+        public void ConsultMoving() => movimientos.ForEach(moves => Console.WriteLine("{1}.- {0}\n", moves, count++));
+
+        public bool VerificacionDeContraseña(string contraseñaIn) => contraseñaIn == contraseña;
+
+    }
+}
---
+++ Cajero/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using System.Linq;
+using System.IO;
+
+
+namespace Cajero
+{
+    class Program
+    {
+        static readonly string pathPass = @"C:\Users\Los Ortegas\source\repos\Cajero\Folder pruebaFile\PwStore";
+        const string
+            menu = "Bienvenido al Cajero",
+            option1 = "Retirar",
+            option2 = "Consultar Saldo",
+            option3 = "Consultar Movimientos",
+            option4 = "Salir";
+        static int intentos = 3;
+        static bool check = false;
+        private static readonly Cajero usuario = new Cajero();
+
+        static void Main(string[] args)
+        {
+
+            Console.CursorSize = 10;
+            Console.SetBufferSize(72, 18);
+            Console.SetWindowSize(80, 20);
+            Console.Title = "Bank Thief International";
+            File.WriteAllText(pathPass, "2315");
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("\n*******Ingrese los 4 digitos de su contraseña*******\n");
+
+                check = usuario.VerificacionDeContraseña(Console.ReadLine());
+                if (check) break;
+
+                intentos--;
+                Console.WriteLine("Contraseña incorrecta.  Intentos restantes {0}.\nPresione Enter para Reintentar.", intentos);
+                Console.ReadKey();
+
+            } while (intentos != 0);
+
+            switch (intentos)
+            {
+                case 0:
+                    Console.WriteLine("\nHa excedido el numero de intentos."); break;
+                default:
+                    Menu();
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("\n\t¡Que tenga un buen dia!");
+                    Console.ReadKey();
+                    break;
+            }
+
+        }
+
+        static void Menu()
+        {
+            ConsoleKeyInfo ckey;
+
+            do
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                // Console.WriteLine("\n***{0}***\n\n1.- {1}\n\n2.- {2}\n\n3.- {3}\n\n4.- {4}\n ", menu, option1, option2, option3, option4);
+                string path = @"C:\Users\Los Ortegas\source\repos\Cajero\Folder pruebaFile\PruebaFile.txt";
+                string blog = File.ReadAllText(path);
+                Console.WriteLine(blog);
+
+                //Console.Write("Seleccione una opcion: [_] ");
+                Console.SetCursorPosition(32, 16);
+                ckey = Console.ReadKey(true);
+
+                if (ckey.Key == ConsoleKey.D1)
+                {
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("\nIngrese el monto de retiro.");
+                    try
+                    {
+                        usuario.Withdraw(double.Parse(Console.ReadLine()));
+                        Console.ReadKey();
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Dato invalido");
+                        Console.ReadKey();
+                    }
+                }
+                else if (ckey.Key == ConsoleKey.D2)
+                {
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("\nIngrese el monto de Deposito. Min( 1$ ) Max( $500 )\n");
+                    try
+                    {
+                        usuario.Deposit(double.Parse(Console.ReadLine()));
+                        Console.ReadKey();
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Dato invalido");
+                        Console.ReadKey();
+                    }
+
+                }
+                else if (ckey.Key == ConsoleKey.D3)
+                {
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.WriteLine("\nSu saldo es: {0:c}\n", usuario.GetSaldo());
+                    Console.ReadKey();
+                }
+                else if (ckey.Key == ConsoleKey.D4)
+                {
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                    usuario.ConsultMoving();
+                    Console.ReadKey();
+                }
+                else if (ckey.Key == ConsoleKey.D6)
+                {
+                    Console.Clear();
+                    Console.ResetColor();
+                    break;
+                }
+
+            } while (ckey.Key != ConsoleKey.D4);
+
+        }
+    }
+
+}
